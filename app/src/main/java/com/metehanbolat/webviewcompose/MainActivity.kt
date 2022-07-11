d802d1cfa6bc292ba8cbd707f6a0966f34a2137b
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-07-11 10:08:59 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/webviewcompose/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.metehanbolat.webviewcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.metehanbolat.webviewcompose.ui.theme.WebViewComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WebViewComposeTheme {
+
+            }
+        }
+    }
+}
